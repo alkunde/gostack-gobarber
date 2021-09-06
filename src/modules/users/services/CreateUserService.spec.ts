60800835ce719954b0b1795f933d68e993e607ea
André Kunde <andre.kunde@zup.com.br>
2021-09-06 19:09:02 -0300

    testing authentication

--- src/modules/users/services/CreateUserService.spec.ts
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -1,13 +1,16 @@
 import AppError from '@shared/errors/AppError';
 
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
 import CreateUserService from './CreateUserService';
 
 describe('CreateUser', () => {
   it('should be able to create a new user', async () => {
     const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
     const createUser = new CreateUserService(
-      fakeUsersRepository
+      fakeUsersRepository,
+      fakeHashProvider,
     );
 
     const user = await createUser.execute({
@@ -21,8 +24,10 @@ describe('CreateUser', () => {
 
   it('should not be able to create a new user with same email', async () => {
     const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
     const createUser = new CreateUserService(
-      fakeUsersRepository
+      fakeUsersRepository,
+      fakeHashProvider,
     );
 
     await createUser.execute({
