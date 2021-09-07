d075dc5d43f9666f3a11bc499db7f403d144a7c4
André Kunde <andre.kunde@zup.com.br>
2021-09-07 18:24:19 -0300

    using handlebars to template emails

--- src/shared/infra/typeorm/migrations/1631028748309-CreateUserTokens.ts
+++ src/shared/infra/typeorm/migrations/1631028748309-CreateUserTokens.ts
@@ -1,4 +1,4 @@
-import { MigrationInterface, QueryRunner, Table } from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 class CreateUserTokens1631028748309 implements MigrationInterface {
 
